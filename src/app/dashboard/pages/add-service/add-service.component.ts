6f1a4ef2bc416790b039d210d0471340dd065920
kennyHR <kennyharry47@gmail.com>
2024-06-06 15:37:08 -0500

    graficos home

--- src/app/dashboard/pages/add-service/add-service.component.ts
+++ src/app/dashboard/pages/add-service/add-service.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-service',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './add-service.component.html',
   styleUrl: './add-service.component.scss'
 })
--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,81 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit{
 
+
+  public chart1: Chart;
+  public chart2: Chart;
+
+
+  ngOnInit(): void {
+  
+    const data1 = {
+      labels: ['Aceptadas', 'Pendientes', 'Recahzadas'],
+      datasets: [{
+        label: 'total',
+        data: [500, 150, 90],
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgb(75, 192, 192)',
+          'rgb(255, 205, 86)',
+          'rgb(255, 99, 132)'
+        ],
+        borderWidth: 1
+      }]
+  }
+
+  this.chart1 = new Chart('chart1', {
+    type: 'bar' as ChartType,
+    data: data1,
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+  const data2 = {
+    labels: ['Aceptadas', 'Pendientes', 'Recahzadas'],
+    datasets: [{
+      label: 'total',
+      data: [1150, 600, 400],
+      backgroundColor: [
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(255, 99, 132)'
+      ],
+      borderWidth: 1
+    }]
+}
+
+this.chart2 = new Chart('chart2', {
+  type: 'bar' as ChartType,
+  data: data2,
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+}
 }
